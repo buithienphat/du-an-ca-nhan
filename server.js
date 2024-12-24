dbf638317685aa4ac045f34a8628875bd786c1ca
Phat <phat@MBP-cua-Phat.lan>
2024-12-24 08:41:36 +0700

    update code

--- server.js
+++ server.js
@@ -28,12 +28,12 @@ app.use("/products", productRoutes);
 // Start the server
 const PORT = process.env.PORT || 50000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
