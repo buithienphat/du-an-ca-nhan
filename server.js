081e7dde6069a8970478d4c1c0bfa672b7360583
Phat <phat@MBP-cua-Phat.lan>
2024-12-26 10:09:26 +0700

    update cors

--- server.js
+++ server.js
@@ -12,24 +12,32 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 // Sử dụng cors với cấu hình mặc định
-const allowedOrigins = [
-  "http://localhost:3000", // Thay bằng port frontend chạy ở localhost
-  process.env.DOMAIN_FE, // Domain trên production
-];
+// const allowedOrigins = [
+//   "http://localhost:3000", // Thay bằng port frontend chạy ở localhost
+//   process.env.DOMAIN_FE, // Domain trên production
+// ];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // Kiểm tra origin có nằm trong danh sách cho phép
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true, // Cho phép gửi cookie
+//   })
+// );
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Kiểm tra origin có nằm trong danh sách cho phép
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: ["http://localhost:3000", "https://client-amber-tau.vercel.app"], // Các domain được phép
     credentials: true, // Cho phép gửi cookie
   })
 );
+
 // Middleware để xử lý JSON
 app.use(express.json());
 
